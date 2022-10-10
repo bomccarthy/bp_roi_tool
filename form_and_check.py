fe5a8279feb5a9a7c3978dceede9909eff9482e9
bomccarthy <110850293+bomccarthy@users.noreply.github.com>
2022-10-10 17:35:43 -0500

    Update form_and_check.py

--- form_and_check.py
+++ form_and_check.py
@@ -1,14 +1,13 @@
 def check(number):
-    if number == 'q':           # if 'q' is entered the program will exit
+    if number == 'q':               # if 'q' is entered the program will exit
         exit()
+    elif number == '':              # if nothing is entered a zero will be returned
+        return 0.0            
     else:
         try:
-            if number == '':          # if nothing is entered a zero will be returned
-                return 0.0              # if somthing is entered a check for float will 
-            else:                       # be returned or asked for re=entry recursively
-                return float(number)
-        except:
-            check(input('This value must be a number without commas. Please re-enter the number or hit "q" to exit.  '))
+            return float(number)    # if somthing is entered a check for float will
+        except:                     # be returned or asked for re=entry recursively
+            return check(input('This value must be a number without commas. Please re-enter the number or hit "q" to exit.  '))
 
 def fmt(num):
     return "$ {:,.2f}".format(num)  # formats to a two decimal number with a '$'
